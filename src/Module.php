0a3da4a604723ce36f1e7dbc1131d357395ad2b8
Sainan <63328889+Sainan@users.noreply.github.com>
2020-08-31 10:46:54 +0200

    Add more functions for calling functions

--- src/Module.php
+++ src/Module.php
@@ -55,23 +55,8 @@ class Module
 		return Kernel32::VirtualAllocEx($this->processHandle, $bytes);
 	}
 
-	/**
-	 * Calls a function in the module that matches void*(*)(uint64_t, uint64_t)
-	 * However, the resulting pointer should point somewhere between the module base and (modBase + pow(2, 32)) as the method of inter process communication that is the thread exit code can only hold 32 bits.
-	 *
-	 * @param int $function_address
-	 * @param int|null $arg_1
-	 * @param int|null $arg_2
-	 * @return Pointer
-	 */
-	function callPtrFunction(int $function_address, ?int $arg_1 = null, ?int $arg_2 = null) : Pointer
+	private function callFunction(int $function_address, ?int $arg_1, ?int $arg_2, int $ret): ?int
 	{
-		$ExitThread_fp = Kernel32::GetProcAddress(Kernel32::GetModuleHandleA("kernel32.dll"), "ExitThread");
-		if ($ExitThread_fp == Pointer::nullptr)
-		{
-			throw new RuntimeException("Failed to find ExitThread");
-		}
-		$dword_base = $this->base->address & 0xFFFFFFFF;
 		$asm = (new AssemblyBuilder())->beginFunction()
 									  ->beginFarCall($function_address);
 		if(is_int($arg_1))
@@ -82,20 +67,88 @@ class Module
 				$asm->setArgument2($arg_2);
 			}
 		}
-		$bytecode = $asm->endFarCall()
-						->subtractFromReturnValue($dword_base)
-						->useReturnValueAsArgument1ToNextCall()
-						->beginFarCall($ExitThread_fp)
-						->endFarCall()
-						->endFunction()
-						->getByteCode();
+		$asm->endFarCall();
+		if($ret != 0)
+		{
+			$ExitThread_fp = Kernel32::GetProcAddress(Kernel32::GetModuleHandleA("kernel32.dll"), "ExitThread");
+			if ($ExitThread_fp == Pointer::nullptr)
+			{
+				throw new RuntimeException("Failed to find ExitThread");
+			}
+			if($ret == 1)
+			{
+				$asm->subtractFromReturnValue($this->base->address & 0xFFFFFFFF);
+			}
+			$asm->useReturnValueAsArgument1ToNextCall()
+				->beginFarCall($ExitThread_fp)
+				->endFarCall();
+		}
+		$asm->endFunction();
+		$bytecode = $asm->getByteCode();
 		$alloc = $this->allocate(strlen($bytecode));
 		$alloc->writeString($bytecode);
 		$thread = Kernel32::CreateRemoteThread($this->processHandle, $alloc->address);
 		Kernel32::WaitForSingleObject($thread);
-		$exit_code = FFI::new("uint32_t");
-		Kernel32::GetExitCodeThread($thread, $exit_code);
-		/** @noinspection PhpUndefinedFieldInspection */
-		return $this->base->add($exit_code->cdata);
+		if($ret != 0)
+		{
+			$exit_code = FFI::new($ret == 3 ? "int32_t" : "uint32_t");
+			Kernel32::GetExitCodeThread($thread, $exit_code);
+			/** @noinspection PhpUndefinedFieldInspection */
+			return $exit_code->cdata;
+		}
+		return null;
+	}
+
+	/**
+	 * Calls a function in the module that accepts 0 to 2 uint64_t-compatible parameters.
+	 *
+	 * @param int $function_address
+	 * @param null|int $arg_1
+	 * @param null|int $arg_2
+	 * @return void
+	 */
+	function callVoidFunction(int $function_address, ?int $arg_1 = null, ?int $arg_2 = null) : void
+	{
+		$this->callFunction($function_address, $arg_1, $arg_2, 0);
+	}
+
+	/**
+	 * Calls a function in the module that accepts 0 to 2 uint64_t-compatible parameters and returns a pointer.
+	 * Note that the resulting pointer should point somewhere between the module base and (modBase + pow(2, 32)) as the method of inter process communication that is the thread exit code can only hold 32 bits.
+	 *
+	 * @param int $function_address
+	 * @param int|null $arg_1
+	 * @param int|null $arg_2
+	 * @return Pointer
+	 */
+	function callPtrFunction(int $function_address, ?int $arg_1 = null, ?int $arg_2 = null) : Pointer
+	{
+		return $this->base->add($this->callFunction($function_address, $arg_1, $arg_2, 1));
+	}
+
+	/**
+	 * Calls a function in the module that accepts 0 to 2 uint64_t-compatible parameters and returns a uint32_t.
+	 *
+	 * @param int $function_address
+	 * @param null|int $arg_1
+	 * @param null|int $arg_2
+	 * @return int
+	 */
+	function callUint32Function(int $function_address, ?int $arg_1 = null, ?int $arg_2 = null) : int
+	{
+		return $this->callFunction($function_address, $arg_1, $arg_2, 2);
+	}
+
+	/**
+	 * Calls a function in the module that accepts 0 to 2 uint64_t-compatible parameters and returns an int32_t.
+	 *
+	 * @param int $function_address
+	 * @param null|int $arg_1
+	 * @param null|int $arg_2
+	 * @return int
+	 */
+	function callInt32Function(int $function_address, ?int $arg_1 = null, ?int $arg_2 = null) : int
+	{
+		return $this->callFunction($function_address, $arg_1, $arg_2, 3);
 	}
 }
