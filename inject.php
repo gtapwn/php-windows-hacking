d7cae0e68497e37ae44e8127ed3203405280c499
Sainan <63328889+Sainan@users.noreply.github.com>
2020-08-31 02:51:14 +0200

    Remove PROCESS_VM_READ from inject.php

--- inject.php
+++ inject.php
@@ -13,7 +13,7 @@ if(empty($argv[2]))
 	die(/** @lang text */ "Syntax: php inject.php <process name> <dll path>\n");
 }
 
-$process = new Process($argv[1], Kernel32::PROCESS_CREATE_THREAD | Kernel32::PROCESS_VM_OPERATION | Kernel32::PROCESS_VM_READ | Kernel32::PROCESS_VM_WRITE);
+$process = new Process($argv[1], Kernel32::PROCESS_CREATE_THREAD | Kernel32::PROCESS_VM_OPERATION | Kernel32::PROCESS_VM_WRITE);
 $parameter = $process->allocate(strlen($argv[2]));
 $parameter->writeString($argv[2]);
 Kernel32::CreateRemoteThread($process->module->processHandle, $LoadLibraryA_fp, $parameter);
